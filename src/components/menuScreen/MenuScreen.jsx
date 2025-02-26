af39cee5220da161bd41a1f0f9b0d2524593c79b
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-02-26 23:21:04 +0900

    [feat] 버튼 클릭 이벤트
    - 헤더 아이콘 변경
    - 메뉴 스크린 페이지 이동 처리 완료
    - input태그 css 수정

--- src/components/menuScreen/MenuScreen.jsx
+++ src/components/menuScreen/MenuScreen.jsx
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as S from './MenuScreenStyle';
+import { useNavigate } from 'react-router-dom';
 
 function MenuScreen({ isOpen, closeMenuScreen }) {
+  const nav = useNavigate();
+
   if (!isOpen) return null; // isOpen이 false면 렌더링 안 함
+
   return (
     <S.Wrapper>
       <S.CancelButton onClick={closeMenuScreen} />
       <S.Title>MENU</S.Title>
       <S.List>
-        <S.Text>서비스명과 채팅하기</S.Text>
-        <S.Text>도전! 결정 장애 극복!</S.Text>
-        <S.Text>나의 결정 장애 극복 랭킹은?</S.Text>
+        <S.Text
+          onClick={() => {
+            nav('/chat');
+            closeMenuScreen();
+          }}
+        >
+          서비스명과 채팅하기
+        </S.Text>
+        <S.Text
+          onClick={() => {
+            nav('/challenge');
+            closeMenuScreen();
+          }}
+        >
+          도전! 결정 장애 극복!
+        </S.Text>
+        <S.Text
+          onClick={() => {
+            nav('/rank');
+            closeMenuScreen();
+          }}
+        >
+          나의 결정 장애 극복 랭킹은?
+        </S.Text>
       </S.List>
     </S.Wrapper>
   );
