f000289d37014484e8b256a172127787161b86e4
hollyPC <hojeongkim23@gmail.com>
2025-02-26 23:49:51 +0900

    added component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'; 
+import BirthInfoForm from './pages/InfoForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <BirthInfoForm />
       </header> 
     </div>
   );
